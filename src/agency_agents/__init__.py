d53f19a1be72483c65ca778f37acb40b8e3a3861
Alex Lau(AvengerMoJo) <avengermojo@gmail.com>
2026-05-11 14:14:15 +0800

    feat: add PersonaModule interface package and module descriptor

---
+++ src/agency_agents/__init__.py
@@ -0,0 +1,5 @@
+"""Agency-agents module interface package."""
+
+from .interface import PersonaModule
+
+__all__ = ["PersonaModule"]
---
+++ src/agency_agents/interface.py
@@ -0,0 +1,7 @@
+"""PersonaModule@1.0 interface adapter for MoJoAssistant integration."""
+
+from __future__ import annotations
+
+from app.roles.persona_provider import AgencyPersonaModule as PersonaModule
+
+__all__ = ["PersonaModule"]
